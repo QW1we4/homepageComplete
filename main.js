ada8a68fc5e7b590c64b63b72943b2392e7144e8
QW1we4 <dkswogh468@gmail.com>
2023-03-22 18:42:32 +0900

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+function getTime() {
+  const time = document.querySelector(".time");
+
+  const newDate = new Date();
+
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getSeconds()).padStart(2, "0");
+  //   let seconds = newDate.getSeconds();
+
+  if (seconds.toString().length === 1) {
+    seconds = "0" + seconds;
+  }
+
+  //   time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
+
+setInterval(getTime, 1000);
